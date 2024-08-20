ff3087730b376f90514552596f8b02745522aa42
Corey Bickford <cb6@live.ca>
2024-08-19 21:14:14 -0700

    Integrate Tailwind CSS and Flowbite into Vite (this took forever)

---
+++ frizl-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// Styling
+import "./App.css";
+import { Button } from "flowbite-react";
+
+export default function App() {
+	return (
+		<>
+			<div>Hello There</div>
+			<Button>Click me</Button>
+		</>
+	);
+}
