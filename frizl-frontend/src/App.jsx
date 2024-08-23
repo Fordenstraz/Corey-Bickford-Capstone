e9027ba994d1c5c03896ebfc74f3731595274cd5
Corey Bickford <cb6@live.ca>
2024-08-22 22:26:30 -0700

    Implement 'Material UI'

--- frizl-frontend/src/App.jsx
+++ frizl-frontend/src/App.jsx
@@ -1,12 +1,17 @@
 // Styling
 import "./App.css";
-import { Button } from "flowbite-react";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+// Components
 
 export default function App() {
 	return (
 		<>
+			<CssBaseline />
 			<div>Hello There</div>
-			<Button>Click me</Button>
 		</>
 	);
 }
--- frizl-frontend/src/main.jsx
+++ frizl-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Imports
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+// Styling
+import "./main.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
