08b90aadf9d01c1c4cd304f18b89fed5deec4b27
Corey Bickford <cb6@live.ca>
2024-08-25 21:40:13 -0700

    Rename 'main' and 'App' from JSX to JS

--- frizl-frontend/src/main.js
+++ frizl-frontend/src/main.js
@@ -1,7 +1,7 @@
 // Imports
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 // Styling
 import "./main.css";
 
