19061eecc265d9dce9476c2222087f641d071567
Corey Bickford <cb6@live.ca>
2024-08-25 21:55:00 -0700

    Setup of initial page routing

--- frizl-frontend/src/App.js
+++ frizl-frontend/src/App.js
@@ -1,3 +1,7 @@
+// Imports
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // Styling
 import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -6,12 +10,50 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 // Components
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
 	return (
 		<>
 			<CssBaseline />
-			<div>Hello There</div>
+			<Router>
+				<Routes>
+					{/* <Route //login page
+						path='/'
+						element={}
+					/>
+
+					<Route //dashboard (year-view)
+						path='/'
+						element={}
+					/>
+
+					<Route //profile page
+						path='/'
+						element={}
+					/>
+
+					<Route //resources page
+						path='/'
+						element={}
+					/>
+
+					<Route //week-view page
+						path='/'
+						element={}
+					/>
+
+					<Route //day-view page
+						path='/'
+						element={}
+					/> */}
+
+					<Route //page not found
+						path='*'
+						element={<PageNotFound />}
+					/>
+				</Routes>
+			</Router>
 		</>
 	);
 }
