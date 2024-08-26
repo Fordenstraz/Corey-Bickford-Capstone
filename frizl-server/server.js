e940e6cfb23d9a923e33bbbc00744fb09f5ea334
Corey Bickford <cb6@live.ca>
2024-08-26 01:26:34 -0700

    Connect routing files to server

--- frizl-server/server.js
+++ frizl-server/server.js
@@ -1,9 +1,23 @@
-require("dotenv").config();
+// Initialize server:
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 const port = process.env.port || 7770;
+require("dotenv").config();
 
+// Enable CORS, and read JSON responses:
+app.use(express.json());
+app.use(cors());
+
+// Routes:
+const userRoutes = require("./routes/userRoutes");
+app.use("/account", userRoutes);
+const plannerRoutes = require("./routes/plannerRoutes");
+app.use("/planner", plannerRoutes);
+const resourceRoutes = require("./routes/resourceRoutes");
+app.use("/resources", resourceRoutes);
+
+// Listen, and confirm:
 app.listen(port, function () {
 	console.log(`Server is running fine on port ${port} 👍`);
 });
