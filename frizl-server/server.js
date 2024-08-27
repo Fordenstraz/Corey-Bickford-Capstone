fde26317ed1c1348c32dad61593f950bffad4b26
Corey Bickford <cb6@live.ca>
2024-08-27 15:29:27 -0700

    Add jsonwebtoken library

--- frizl-server/server.js
+++ frizl-server/server.js
@@ -1,23 +1,34 @@
 // Initialize server:
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = process.env.port || 7770;
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const app = express();
+
+// Environment:
+const PORT = process.env.PORT || 5555;
 
 // Enable CORS, and read JSON responses:
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // Routes:
 const userRoutes = require("./routes/userRoutes");
 app.use("/account", userRoutes);
+
 const plannerRoutes = require("./routes/plannerRoutes");
 app.use("/planner", plannerRoutes);
+
 const resourceRoutes = require("./routes/resourceRoutes");
 app.use("/resources", resourceRoutes);
 
+// Error handling:
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.status(500).send("Uh oh! There was an error connecting to the server");
+});
+
 // Listen, and confirm:
-app.listen(port, function () {
-	console.log(`Server is running fine on port ${port} 👍`);
+app.listen(PORT, function () {
+	console.log(`FRIZL is running fine, on port ${PORT} 👍`);
 });
