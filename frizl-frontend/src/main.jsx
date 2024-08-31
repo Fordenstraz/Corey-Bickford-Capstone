dcfdffc7bf111cf7d53095252bcc62d3a6e1c8bc
Corey Bickford <cb6@live.ca>
2024-08-31 13:32:04 -0700

    Add Google OAuth dependency and apply in main.jsx

--- frizl-frontend/src/main.jsx
+++ frizl-frontend/src/main.jsx
@@ -2,10 +2,15 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const googleClientId =
+	"465419946579-o8m2c35d77rdpqu5f7l41gu46nbj8lr4.apps.googleusercontent.com";
 
 // Render App:
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<GoogleOAuthProvider clientId={googleClientId}>
+			<App />
+		</GoogleOAuthProvider>
 	</StrictMode>
 );
