9c32eb22da37d8926d4e9021b8c09cc0a2f39cbf
Corey Bickford <cb6@live.ca>
2024-09-01 14:36:27 -0700

    Add logic to show current menu selection

--- frizl-frontend/src/components/Navbar/Navbar.jsx
+++ frizl-frontend/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,51 @@
 // Import
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 // Styling
 import "./Navbar.scss";
 
 export default function BottomNavBar() {
+	// Logic for highlighting current menu option:
+	const view = useLocation(); // enables path checking
+	// current view is held in state:
+	const [activeView, setActiveView] = useState(view.pathname);
+	// update state when a new view is selected:
+	useEffect(() => {
+		setActiveView(view.pathname);
+	}, [view.pathname]);
+	// set the selected view as 'currentView':
+	const currentView = path => activeView === path;
+
 	return (
 		<div className="menu-bar">
 			<nav className="menu-bar__nav">
 				<Link
 					to="/dashboard/my_year"
-					className="menu-bar__nav-item">
+					className={`menu-bar__nav-item ${
+						currentView("/dashboard/my_year")
+							? "menu-bar__nav-item--selected"
+							: ""
+					}`}>
 					My Year
 				</Link>
 
 				<Link
 					to="/dashboard/my_week"
-					className="menu-bar__nav-item">
+					className={`menu-bar__nav-item ${
+						currentView("/dashboard/my_week")
+							? "menu-bar__nav-item--selected"
+							: ""
+					}`}>
 					My Week
 				</Link>
 
 				<Link
 					to="/dashboard/my_day"
-					className="menu-bar__nav-item">
+					className={`menu-bar__nav-item ${
+						currentView("/dashboard/my_day")
+							? "menu-bar__nav-item--selected"
+							: ""
+					}`}>
 					My Day
 				</Link>
 			</nav>
