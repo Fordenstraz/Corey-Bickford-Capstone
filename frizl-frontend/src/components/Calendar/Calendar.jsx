7870d19c7a2c1c91bee0ace5cd23ba501029b95c
Corey Bickford <cb6@live.ca>
2024-09-02 18:49:17 -0700

    Initial component setup, and styling

--- frizl-frontend/src/components/Calendar/Calendar.jsx
+++ frizl-frontend/src/components/Calendar/Calendar.jsx
@@ -1,34 +1,54 @@
+// Import
+import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+// Styling
+import "./Calendar.scss";
 
-export default function Calendar() {
+// View plugins:
+const plugins = [dayGridPlugin, listPlugin, interactionPlugin];
+
+// Temporary array of event objects:
+const events = [
+	{ title: "event 1", date: "2019-04-01" },
+	{ title: "event 2", date: "2019-04-02" },
+];
+
+export default function Calendar({ view }) {
+	// Use state to toggle if weekends are displayed:
+	const [toggleWeekends, setToggleWeekends] = useState(true);
+
+	// Call to server for events:
+	// to be handled later
+
 	//handler for clicking on a day:
 	const handleDateClick = arg => {
-		alert(arg.dateStr);
+		//add logic here:
 	};
 
 	return (
-		<FullCalendar
-			plugins={[dayGridPlugin]}
-			//initialView decides what calendar view to use (year, month, week, etc)
-			initialView='dayGridMonth'
-			//select whether weekends will be displayed
-			weekends={false}
-			//enter calendar events
-			events={[
-				{ title: "event 1", date: "2019-04-01" },
-				{ title: "event 2", date: "2019-04-02" },
-			]}
-			//date click handler
-			dateClick={handleDateClick}
-			//event content rendering
-			eventContent={renderEventContent}
-		/>
+		<div className="calendar-container">
+			<FullCalendar
+				plugins={plugins}
+				//what calendar view to use (year, month, week, day)
+				initialView={view}
+				//whether weekends will be displayed
+				weekends={toggleWeekends}
+				//enter calendar events
+				events={events}
+				//date click handler
+				dateClick={handleDateClick}
+				//event content rendering
+				eventContent={showEventDetails}
+			/>
+		</div>
 	);
 }
 
 // Additional function created to render event content:
-function renderEventContent(eventInfo) {
+function showEventDetails(eventInfo) {
 	return (
 		<>
 			<b>{eventInfo.timeText}</b>
